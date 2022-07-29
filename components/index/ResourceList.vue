2ee340be9eae655f2fae757fbbdb673ac6fc63e1
hxh <1246996371@qq.com>
2022-07-29 14:10:34 +0800

    update ui

--- components/index/ResourceList.vue
+++ components/index/ResourceList.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
         <!-- 搜索 -->
-        <HomeList v-if="!category"></HomeList>
-
-        <div v-else class="mt-8">
-            <!-- <ListBar></ListBar> -->
-            <div class="d-flex flex-wrap justify-space-between mt-5">
-                <ResourceCard v-for="item in resources" :key="item.value" :resource="item"></ResourceCard>
+        <!-- <HomeList v-if="!category"></HomeList> -->
+        <div style="height: 100vh" class="d-flex flex-column py-8">
+            <div class="flex-grow-1 pr-3" style="overflow-y: auto; height: 0;">
+                <!-- <ListBar></ListBar> -->
+                <div class="d-flex flex-wrap justify-space-between mb-8">
+                    <ResourceCard v-for="item in resources" :key="item.value" :resource="item"></ResourceCard>
+                </div>
             </div>
         </div>
     </div>
@@ -36,22 +37,29 @@ export default {
         }
     },
     computed: {
-        resources() {
+        allResoucrs() {
             let items = [];
-             for(const path in modules) {
+            for(const path in modules) {
                 const content = modules[path].default;
                 const pathArr = path.split('/');
                 pathArr.pop();
                 const value = pathArr.pop();
                 const category = pathArr.pop();
-                if (category === this.category) {
-                    items.push({
-                        ...content,
-                        category,
-                        value
-                    });
-                }
+                items.push({
+                    ...content,
+                    category,
+                    value
+                });
              }
+            return items;
+        },
+        resources() {
+            let items = [];
+            if (this.category) {
+                items = this.allResoucrs.filter(item => item.category === this.category);
+            } else {
+                items = this.allResoucrs.slice(0, 3);
+            }
             const fillup = 3 - (items.length % 3 || 3);
             if (fillup > 0) {
                 items = [
