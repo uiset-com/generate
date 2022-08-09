df21a5260375e552d3b8a0bd9e36048b3469116d
hxh <1246996371@qq.com>
2022-08-09 11:15:02 +0800

    add summary task

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="pt-5">
         <div class="pa-5 d-flex flex-column justify-center align-center">
             <div class="font-weight-bold" style="font-size: 35px"><span style="color: #E14C50">UI</span>SET</div>
-            <div class="mt-3">收集免费优质的UI资源</div>
+            <div class="mt-3">共收集 <span class="font-weight-bold">{{ summary.total_resources }}</span> 个免费优质的 UI 资源</div>
             <div style="width: 50%" class="mt-10">
                 <SearchInput placeholder="搜索插图 / 图标 / 图案等" height="50"></SearchInput>
             </div>
@@ -14,10 +14,11 @@
 </template>
 
 <script>
-
+import summary from '../data/summary.json';
 export default {
     data() {
         return {
+            summary,
             latests: {}
         };
     },
